2217861fc65035ccb55888f8a8c95aa4add860d1
kvasnica <kvasnica@localhost>
2005-05-12 07:25:34 +0000

    [PATCH} use hand-written interior point solver
    uses a hand-written interior point method solver to solve the
    analytic center problem, instead of logarithm maximization
    (by Miroslav Baric)

--- @polytope/analyticCenter.m
+++ @polytope/analyticCenter.m
@@ -1,4 +1,4 @@
-function x = analyticCenter(P)
+function x = analyticCenter(P,Options)
 %ANALYTICCENTER Computes an analytic center of a polytope
 %
 % x = center(P)
@@ -7,13 +7,19 @@ function x = analyticCenter(P)
 % DESCRIPTION
 % ---------------------------------------------------------------------------
 % Computes an analytic center of a polytope. The input polytope must be bounded
-% and fully dimensional! An SDP programming based technique is used, YALMIP is
-% required.
+% and fully dimensional! Newton iteration based algorithm is used.
 %
 % ---------------------------------------------------------------------------
 % INPUT
 % ---------------------------------------------------------------------------
-% P        - Polytope
+% P                - Polytope
+% Options.x0       - a point inside relative interior of the polytope P
+%                    (default: Chebyshev center)
+% Options.tol      - tolerance used as a stopping criterion, corresponds
+%                    to the step of the Newton iteration
+%                    (default: Chebyshev radius/10)   
+% Options.maxIter  - maximal number of Newton step iterations 
+%                    (default 10)    
 %
 % ---------------------------------------------------------------------------
 % OUTPUT                                                                                                    
@@ -23,10 +29,12 @@ function x = analyticCenter(P)
 % see also CHEBYBALL
 %
 
-% $Id: analyticCenter.m,v 1.2 2005/03/01 11:16:43 kvasnica Exp $
+% $Id: analyticCenter.m,v 1.3 2005/05/12 09:25:34 kvasnica Exp $
 %
-% (C) 2004 Michal Kvasnica, Automatic Control Laboratory, ETH Zurich,
+% (C) 2005 Miroslav Baric, Automatic Control Laboratory, ETH Zurich,
 %          kvasnica@control.ee.ethz.ch
+% (C) 2005 Johan Loefberg, Automatic Control Laboratory, ETH Zurich,
+%          loefberg@control.ee.ethz.ch
 
 % ---------------------------------------------------------------------------
 % Legal note:
@@ -58,14 +66,6 @@ if ~isempty(P.Array),
     error('ANALYTICCENTER: This function does not work with polytope arrays!');
 end
 
-if ~isminrep(P)
-    P = reduce(P);
-end
-
-if ~isnormal(P)
-    P = normalize(P);
-end
-
 if ~isfulldim(P)
     error('ANALYTICCENTER: Polytope must be fully dimensional!');
 end
@@ -74,23 +74,89 @@ if ~isbounded(P)
     error('ANALYTICCENTER: Polytope must be bounded!');
 end
 
-nx = dimension(P);
-nc = nconstr(P);
+if ~isminrep(P)
+    P = reduce(P);
+end
 
-xsdp = sdpvar(nx,1);
-G = diag(-(P.H*xsdp-P.K));
+if ~isnormal(P)
+    P = normalize(P);
+end
 
-options = mptOptions.sdpsettings;
+xCheby = P.xCheb;
+rCheby = P.RCheb;
 
-solution=solvesdp(set(G>0), -logdet(G), options);
 
-if solution.problem==0
-    % solution is feasible
-    x = double(xsdp);
-elseif solution.problem==4
-    % numerical problems
-    warning('ANALYTICCENTER: Numerical problems');
-    x = double(xsdp);
+if ( nargin == 1 ),
+    % using default settings
+    nIter = 10;
+    EPS = rCheby / 10;
+    x0 = xCheby;
 else
-    error('ANALYTICCENTER: Problem is infeasible!');
+    if ~isfield(Options,'x0'),
+        %
+        % use Chebyshev center by default
+        %
+        x0 = xCheby;
+    else
+        x0 = Options.x0;
+        if ~isinside(P,x0),
+            warning(['ANALYTICCENTER: specified initial point is not inside the ' ...
+                     'polytope, using Chebyshev center.']);
+            x0 = xCheby;
+        end
+    end
+    
+    if ~isfield(Options,'tol'),
+        EPS = rCheby/10;
+    else
+        EPS = Options.tol;
+    end
+    
+    if ~isfield(Options,'nIter'),
+        nIter = 10;
+    else
+        nIter = Options.maxIter;
+    end
+end
+
+ALPHA = 0.2;
+BETA  = 0.717;
+
+[A,b]   = double(P); 
+x       =  x0;
+nablaf  = -A'*(1./(b-A*x));
+Hessian =  A'*diag((b-A*x).^(-2))*A;
+fk      = -sum(log(b-A*x));
+
+while ( (nablaf'*(Hessian\nablaf) > 2*EPS) & (nIter > 0) )
+    dx = Hessian\nablaf;
+       
+    % initial step length (maximum step which keeps us within the bounds of
+    % the polytope)
+    %
+    inProd = A * dx;
+    idxGTZero = find(inProd > 0);
+    inProd = inProd(idxGTZero); % take only > 0
+    slacks = b(idxGTZero) - A(idxGTZero,:) * x;
+    t0 = min ( slacks * norm(dx) ./ inProd );
+    t0 = min(0.99*t0,1);
+    
+    % do the backtracking linesearch
+    %
+    t = t0;
+    while ( 1 ),
+        fkpp   = -sum(log(b-A*(x+t*dx)));
+        if ( (fk - fkpp + ALPHA*t*nablaf'*dx) > 0 ),
+            break;
+        end
+        t = BETA * t;
+    end
+    x = x + t*dx;
+
+    fk     = -sum(log(b-A*x));
+    s      = 1./(b-A*x);
+    nablaf = -A'*s;
+    SA = diag(s)*A;    
+    Hessian = SA'*SA;    
+    nIter = nIter - 1;
 end
