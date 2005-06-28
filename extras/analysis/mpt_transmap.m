881f61e8c6aae02bc59e5da618681dcecf209d64
kvasnica <kvasnica@localhost>
2005-06-28 08:46:04 +0000

    [PATCH] updated transition pruning using Johan's algorithm
    Signed-off-by: Johan Loefberg
    Signed-off-by: Michal Kvasnica

--- extras/analysis/mpt_transmap.m
+++ extras/analysis/mpt_transmap.m
@@ -1,35 +1,33 @@
-function [tmap, P1, P2]=mpt_transmap(P1, A, f, P2, Options)
+function [tmap,Pn,ex,explus] = mpt_transmap(Pn, Acell, fcell, Options)
 %MPT_TRANSMAP Computes transition map
 %
 % ---------------------------------------------------------------------------
 % DESCRIPTION
 % ---------------------------------------------------------------------------
-% Computes map of transitions which are NOT feasible. Specifically:
-%   domain(P2(j), A{i}, f{i}, P1(i)) is NOT feasible if tmap(i, j) is non-zero
+% Prunes infeasible transitions. Specifically:
+%   domain(P2(j), A{i}, f{i}, P1(i)) is NOT feasible if tmap(i, j) is 0
 %
 % ---------------------------------------------------------------------------
 % INPUT
 % ---------------------------------------------------------------------------
-% P1          - polytope array
+% Pn          - polytope array
 % A, f        - cells containing affine map: x(k+1) = A{i}*x + f{i}
-% P2          - polytope array
-% Options.maxsplanes - maximum number of generated separating hyperplanes when
-%                      computing transition map (default is 1000)
 %
 % ---------------------------------------------------------------------------
 % OUTPUT                                                                                                    
 % ---------------------------------------------------------------------------
 % tmap        - sparse matrix with non-zero elements on indexes which correspond
-%               to transitions whiche are NOT FEASIBLE. e.g.:
-%               tmap(i, j) means domain(P2(j), A, f, P1(j)) does NOT exist
-% P1          - input polyarray P1 with extreme points stored inside for each
+%               to transitions whiche are POSSIBLY feasible.
+% Pn          - input polyarray P1 with extreme points stored inside for each
 %               polytope
-% P2          - input polyarray P2 with extreme points stored inside for each
-%               polytope
+% ex          - extreme points of Pn as a cell array
+% explus      - extreme points of the affine transformation A*x+f
 %
 
-% $Id: mpt_transmap.m,v 1.1 2005/06/27 20:19:42 kvasnica Exp $
+% $Id: mpt_transmap.m,v 1.2 2005/06/28 10:46:04 kvasnica Exp $
 %
+% (C) 2005 Johan Loefberg, Automatic Control Laboratory, ETH Zurich,
+%          joloef@control.ee.ethz.ch
 % (C) 2005 Michal Kvasnica, Automatic Control Laboratory, ETH Zurich,
 %          kvasnica@control.ee.ethz.ch
 
@@ -57,187 +55,155 @@ global mptOptions
 if ~isstruct(mptOptions),
     mpt_error;
 end
-
-error(nargchk(3,5,nargin));
-
-if nargin < 5,
-    Options = [];
+if ~isfield(Options, 'lpsolver'),
+    Options.lpsolver = mptOptions.lpsolver;
 end
-p1p2equal = 0;
-if nargin==4,
-    if isa(P2, 'struct'),
-        Options = P2;
-        P2 = P1;
-        p1p2equal = 1;
+if ~isfield(Options, 'abs_tol'),
+    Options.abs_tol = mptOptions.abs_tol;
+end
+
+lpsolver = Options.lpsolver;
+abs_tol = Options.abs_tol;
+
+lenP = length(Pn);
+tmap = ones(lenP,lenP);
+
+% Vertex enumeration
+for is=1:lenP
+    [E, R, Pn(is)] = extreme(Pn(is));
+    ex{is} = E';
+    
+    % round almost-zero elements
+    ex{is}(find(abs(ex{is}) < 1e-12)) = 0;
+    explus{is} = Acell{is}*ex{is} + repmat(fcell{is},1,size(ex{is},2));
+    
+    % round almost-zero elements
+    explus{is}(find(abs(explus{is}) < 1e-12)) = 0;
+end
+
+n = dimension(Pn);
+
+for i=1:lenP
+
+    BoxMin{i} = min(ex{i}')';
+    BoxMax{i} = max(ex{i}')';
+    
+    %Pn(i) = set(Pn(i),'bbox',[BoxMin{i} BoxMax{i}]);
+
+    %now extract all other extreme points of the bounding box
+    for j=1:2^n
+        index=dec2bin(j-1,n);
+        for k=1:n
+            if(index(k)=='1')
+                boxPoint{i}{j}(k)=BoxMax{i}(k);
+            else
+                boxPoint{i}{j}(k)=BoxMin{i}(k);
+            end
+        end%for k=1:n
+        if(size(boxPoint{i}{j},1)<size(boxPoint{i}{j},2))
+            boxPoint{i}{j}= boxPoint{i}{j}';
+        end
+    end%for j=1:2^(n)
+end%Pn
+
+
+% Initial prune, based on coordinate cuts
+% Typically removes 10%
+for r = 1:n
+    a = eyev(n,r);
+    b = 0;
+    for prunei = 1:lenP
+        iprunes(prunei) = all((a'*explus{prunei} < -1e3*abs_tol));
     end
-end
-if nargin < 4,
-    p1p2equal = 1;
-    P2 = P1;
-    Options = [];
-end
-
-if ~isfield(Options, 'maxsplanes'),
-    % gives maximum number of auto-generated separating hyperplanes
-    Options.maxsplanes = 1000;
-end
-if isfield(Options, 'p1p2equal'),
-    p1p2equal = Options.p1p2equal;
-end
-
-maxsplanes = Options.maxsplanes;
-lpsolver = mptOptions.lpsolver;
-abs_tol = mptOptions.abs_tol;
-
-if ~isa(P1, 'polytope'),
-    error('First input must be a polytope object.');
-end
-if dimension(P1) ~= dimension(P2),
-    error('Dimensions of polytopes must be identical.');
-end
-if ~isa(P2, 'polytope'),
-    error('Fourth input must be a polytope object.');
-end
-
-lenP1 = length(P1);
-lenP2 = length(P2);
-if length(A) ~= lenP1,
-    error('Length of A must equal length of P1.');
-end
-if length(f) ~= lenP1,
-    error('Length of f must equal length of P1.');
-end
-
-EP1 = cell(1, lenP1);
-EP2 = cell(1, lenP2);
-
-% compute extreme points of each polytopes
-for ii = 1:lenP1,
-    [EP1{ii}, R, P1(ii)] = extreme(P1(ii));
-end
-if p1p2equal,
-    % if P1 == P2, don't recompute extreme points twice
-    EP2 = EP1;
-    P2 = P1;
-else
-    for ii = 1:lenP2,
-        [EP2{ii}, R, P2(ii)] = extreme(P2(ii));
+    for prunej = 1:lenP
+        jprunes(prunej) = all((a'*ex{prunej} > 1e3*abs_tol));
     end
-end
-
-% compute bounding boxes of the two polyarrays
-[R, bbox1_low, bbox1_up] = bounding_box(P1, struct('noPolyOutput', 1));
-if p1p2equal,
-    bbox2_low = bbox1_low;
-    bbox2_up = bbox1_up;
-else
-    [R, bbox2_low, bbox2_up] = bounding_box(P1, struct('noPolyOutput', 1));
-end
-
-% compute initial separating hyperplanes by taking the bounding box, computing
-% it's center and taking axis through the center orthogonal to x1, x2, ..., xn
-dim = dimension(P1);
-bbox1_center = (bbox1_up - bbox1_low) / 2;
-bbox2_center = (bbox2_up - bbox2_low) / 2;
-
-splanes = [];
-for idim = 1:dim,
-    % each hyperplane is of the form [A b], where A*x <= b
-    oneplane = [zeros(1, dim) bbox1_center(dim)];
-    oneplane(idim) = 1;
-    splanes = [splanes; oneplane];
-end
-
-% if P1 ~= P2 and their bounding boxes are not identical, include also
-% separating hyperplanes based on center of the second bounding box
-if ~p1p2equal | any(bbox1_center ~= bbox2_center),
-    for idim = 1:dim,
-        % each hyperplane is of the form [A b], where A*x <= b
-        oneplane = [zeros(1, dim) bbox2_center(dim)];
-        oneplane(idim) = 1;
-        splanes = [splanes; oneplane];
+    for prunei = 1:lenP
+        for prunej = 1:lenP
+            if iprunes(prunei)
+                if jprunes(prunej)
+                    tmap(prunei,prunej) = 0;
+                end
+            end
+        end
     end
 end
 
-newsplanescount = 0;
+for i = 1:lenP
+    isfulldimP(i) = isfulldim(Pn(i));   
+end
 
-% alocate the transition map as a sparce matrix initialized to 0
-% tmap(index_p1, index_p2) will be 1 if NO transition exists
-tmap = spalloc(lenP1, lenP2, lenP1*lenP2);
+vecex = [ex{1:end}];
+vecexplus = [explus{1:end}];
+indiciesex = cumsum([1 cellfun('prodofsize',ex)/n]);
+indiciesexplus = cumsum([1 cellfun('prodofsize',explus)/n]);
+lpsolver = Options.lpsolver;
 
-% now try to rule out as many transitions as possible
-for i1 = 1:lenP1,
-    P1_v = EP1{i1};
-    % affine transformation of polytope P1(i1)
-    nv = size(EP1{i1}, 1);
-    P1_v = (A{i1}*P1_v' + repmat(f{i1}, 1, nv))';
-    for i2 = 1:lenP2,
-        % vertices of the second polytope
-        P2_v = EP2{i2};
+%neighbour = ones(lenP,lenP);
 
-        if newsplanescount < maxsplanes,
-            % compute new separating hyperplane
-            [sephp, xopt] = sub_sephp(P1_v, P2_v, lpsolver);
-            if sephp,
-                % separating hyperplane found, add it to the stack
-                splanes = [splanes; (xopt(:))'];
-                newsplanescount = newsplanescount + 1;
-                
-                % moreover we know that no transtion exists, since separating
-                % hyperplane can be found
-                tmap(i1, i2) = 1;
-                
-                continue
+for i=1:lenP
+
+    %check if polytope is mapped onto a full dimensional polytope or onto
+    %a facet (this has an impact on the subsequent reachability analysis)
+    if(all(abs(eig(Acell{i}))>Options.abs_tol))
+        fullMap=1;
+    else
+        fullMap=0;
+    end
+
+    if fullMap,
+        lowerCur = min((Acell{i} * [boxPoint{i}{:}] + repmat(fcell{i},1,length(boxPoint{i})))')';
+        upperCur = max((Acell{i} * [boxPoint{i}{:}] + repmat(fcell{i},1,length(boxPoint{i})))')';
+    end
+
+    for j = find(tmap(i,:))
+
+        if tmap(i,j)
+            if (fullMap)
+                if any((upperCur < BoxMin{j}) | (lowerCur>BoxMax{j}))
+                    tmap(i,j) = 0;
+                end
+            else
             end
         end
 
-        % check if sets of points are separated by an existing separating
-        % hyperplane
-        separated = sub_areseparated(P1_v, P2_v, splanes, abs_tol);
-        if separated,
-            % yes, points are separated, hence no transition between these two
-            % sets of points exists 
-            tmap(i1, i2) = 1;
-            continue
+        if tmap(i,j)
+
+            [a,b,f] = separatinghp(ex{j},explus{i},lpsolver,n);
+
+            if (f == 1) % Found one!
+                AA = a'*vecexplus;
+                BB = a'*vecex;
+                iprunes  = zeros(lenP,1);
+                for prunei = i:lenP
+                    iprunes(prunei) = all((AA(indiciesexplus(prunei):indiciesexplus(prunei+1)-1)-b)>=0);
+                end
+                jprunes  = zeros(lenP,1);
+                for prunej = 1:lenP
+                    jprunes(prunej) = all((BB(indiciesex(prunej):indiciesex(prunej+1)-1)-b)<=0);
+                end
+                jprunes = logical(jprunes);
+                if any(jprunes)
+                    for prunei = find(iprunes)
+                        tmap(prunei,jprunes) = 0;
+                    end
+                end
+            end
+
         end
-        
     end
 end
 
-%----------------------------------------------------------------------
-function result = sub_areseparated(V1, V2, hypplanes, abs_tol)
-% checks if sets of points V1 and V2 are separated by a hyperplane
+%-------------------------------------------------------------------
+function [a,b,how] = separatinghp(X,Y,lpsolver,nx)
 
-result = 0;
-nv1 = size(V1, 1);
-nv2 = size(V2, 1);
-V1 = V1';
-V2 = V2';
-for iplanes = 1:size(hypplanes, 1),
-    % check all hyperplanes
-    hplane = hypplanes(iplanes, :);
-    a = hplane(1:end-1);
-    b = hplane(end);
-    if all(a*V1 - b <= abs_tol),
-        % points V1 are strictly on the positive side of this hyperplane
-        if all(-a*V2 + b <= abs_tol),
-            % points V2 are strictly on the negative side of this hyperplane
-            result = 1;
-            return
-        end
-    end
-end        
+mx = size(X,2);
+my = size(Y,2);
 
-
-%----------------------------------------------------------------------
-function [sephp, xopt] = sub_sephp(V1, V2, lpsolver)
-
-% try to find a separating hyperplane between P and Pn
-[nv1, nx] = size(V1);
-nv2 = size(V2, 1);
-f = -ones(1,nx+1);
-A = [V1 -ones(nv1,1); -V2 ones(nv2, 1); zeros(1, nx) 1];
-B = [zeros(nv1+nv2, 1); 1];
-[xopt,fval,lambda,exitflag,how]=mpt_solveLPi(f,A,B,[],[],[],lpsolver,0);
-
-sephp = ~all(abs(xopt<1e-8)) & ~any(xopt==1e9);
+A = [X' -ones(mx,1);-Y' ones(my,1)];
+b = [-ones(mx,1);-ones(my,1)];
+f = zeros(1,nx+1);
+[xopt,fval,lambda,exitflag,how]=mpt_solveLPi(f,A,b,[],[],[],lpsolver,0);
+a = xopt(1:nx);
+b = xopt(end);
+how = ~all(abs(xopt<1e-8)) & ~any(xopt==1e9);
