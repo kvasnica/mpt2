fd430d5efcaaea1c50b6a84e334d13b805833c91
Michal Kvasnica <kvasnica@control.ee.ethz.ch>
2006-02-08 11:51:06 +0100

    sub_computeTrajectory: handle open-loop solutions for PWA systems
    if an explicit solution for PWA systems has been computed by
    mpt_yalmipcftoc(), the full open-loop solution can be stored
    directly in ctrl.Fi and ctrl.Gi. therefore we now handle this
    case when computing open-loop trajectories.

--- extras/auxiliary/sub_computeTrajectory.m
+++ extras/auxiliary/sub_computeTrajectory.m
@@ -243,9 +243,13 @@ if Options.openloop,
     end
     x0 = extend_x0(ctrl, x0, u_prev, Options.reference, isEXPctrl, isDUmode);
     if isEXPctrl,
-        if iscell(sysStruct.A),
+        if iscell(sysStruct.A) & size(ctrl.Fi{1}, 1)<=nu,
             % we have a PWA system, call mpt_computeTrajectory which calculates
-            % open-loop trajectory for PWA system
+            % open-loop trajectory for PWA system.
+            %
+            % however if mpt_yalmipcftoc() was used to compute the open-loop
+            % solution, it is stored directly in ctrl.Fi and ctrl.Gi terms as in
+            % case of linear systems
             [X,U,Y,D,cost,trajectory,feasible,dyns] = mpt_computeTrajectory(struct(ctrl), x0, N, Options);
             return
         end
