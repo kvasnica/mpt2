e9720d5cd5ff61537e215ffce62dfee9f3e19fd4
Michal Kvasnica <kvasnica@control.ee.ethz.ch>
2006-02-16 11:29:07 +0100

    mpt_getCommonSOSLyapFct: fix wrong boolean condition and indexing
    reported by A. Beccuti.

--- extras/analysis/mpt_getCommonSOSLyapFct.m
+++ extras/analysis/mpt_getCommonSOSLyapFct.m
@@ -939,7 +939,7 @@ for ii = 1:nRegions,
         % if the region contains the origin, it has to be the
         % equilibrium point
         %
-        if ( all(abs(bigBG(rowRange))) < Options.epsilon ),
+        if ( all(abs(bigBG(rowRange)) < Options.epsilon )),
             idxContainsOrigin(end+1) = ii;
         end
     end
@@ -976,7 +976,7 @@ if ( isNoisy ),
             for jj = idxContainsOrigin,
                 auxIntersect = intersect(Pnorig(ii),Pn(jj));
                 if ( ~isempty(auxIntersect) ),
-                    newPnorig(end+1) = auxIntersect;
+                    newPnorig = [newPnorig auxIntersect];
                     auxDyn(end+1) = jj;
                 end
             end
