9f2341ab6c6713519fbda65e74a4066bb8c6f300
Michal Kvasnica <kvasnica@control.ee.ethz.ch>
2006-12-04 11:53:12 +0100

    Bug 66: mpt_feasibleStates() should use polytope/grid()
    Use polytope/grid() to obtain list of points contained in
    a given polytope.
    
    unit test: regress/extras/auxiliary/mpt_feasibleStates_test1.m
    unit test: regress/extras/auxiliary/mpt_feasibleStates_test2.m

--- extras/auxiliary/mpt_feasibleStates.m
+++ extras/auxiliary/mpt_feasibleStates.m
@@ -1,7 +1,7 @@
 function X0 = mpt_feasibleStates(ctrl,gridpoints,Options)
 %MPT_FEASIBLESTATES returns equidistantly spaced data points in feasible set
 %
-% X0 = mpt_feasibleStates(ctrl,gridpoints,Options)
+% X0 = mpt_feasibleStates(ctrl, gridpoints, Options)
 %
 % ---------------------------------------------------------------------------
 % DESCRIPTION
@@ -24,15 +24,14 @@ function X0 = mpt_feasibleStates(ctrl,gridpoints,Options)
 % ---------------------------------------------------------------------------
 % OUTPUT
 % ---------------------------------------------------------------------------
-% X0     - set of feasible initial states (states which lie in
-%          ctrl.Pn)
+% X0     - set of feasible initial states (states contained in ctrl.Pn)
 %
-% see also MPT_PERFORMANCE
+% see also MPT_PERFORMANCE, POLYTOPE/GRID
 %
 
 % Copyright is with the following author(s):
 %
-%(C) 2004-2005 Michal Kvasnica, Automatic Control Laboratory, ETH Zurich,
+%(C) 2004-2006 Michal Kvasnica, Automatic Control Laboratory, ETH Zurich,
 %              kvasnica@control.ee.ethz.ch
 
 % ---------------------------------------------------------------------------
@@ -56,18 +55,7 @@ function X0 = mpt_feasibleStates(ctrl,gridpoints,Options)
 % ---------------------------------------------------------------------------
 
 error(nargchk(1,3,nargin));
-
-global mptOptions;
-
-if ~isstruct(mptOptions),
-    mpt_error;
-end
-
-if nargin<1,
-    error('mpt_feasibleStates: Wrong number of input arguments!');
-end
-
-if nargin<3,
+if nargin < 3,
     Options = [];
 end
 
@@ -75,123 +63,45 @@ if isa(ctrl, 'mptctrl')
     if ~isexplicit(ctrl)
         error('This function supports only explicit controllers!');
     end
-    ctrlStruct = struct(ctrl);
+    x0format = ctrl.details.x0format;
+    nx = x0format.required - x0format.reference - x0format.uprev;
+    
+elseif isstruct(ctrl),
+    if mpt_isValidCS(ctrl)
+        nx = mpt_sysStructInfo(ctrl.details.origSysStruct);
+    else
+        error('First argument has to be a valid controller structure!');
+    end
+    
 else
-    ctrlStruct = ctrl;
-end
-
-if ~isfield(Options,'verbose')
-    Options.verbose=mptOptions.verbose;
+    error('Unknown type of first input argument.');
+    
 end
 
 if nargin>1 & ~isa(gridpoints,'double')
     error('Second input argument must be number of grid points!');
 end
 
-if ~mpt_isValidCS(ctrlStruct)
-    error('mpt_feasibleStates: First argument has to be a valid controller structure! See mpt_control for details.');
-end
-
-if nargin<2
+if nargin < 2
     gridpoints = 30;
-    if Options.verbose>0,
-        disp('mpt_feasibleStates: Number of grid points not given, assuming 30');
-    end
 end
 
-if isfield(Options,'Pfinal') %& ~isfulldim(ctrlStruct.Pfinal),
-    if ~isa(Options.Pfinal,'polytope'),
-        error('mpt_performance: Options.Pfinal must be a polytope object!');
+if isfield(Options, 'Pfinal'),
+    if ~isa(Options.Pfinal, 'polytope'),
+        error('Options.Pfinal must be a polytope object!');
     end
-    ctrlStruct.Pfinal = Options.Pfinal;
-end
-
-% first compute bounds on feasible state-space
-if ~isfulldim(ctrlStruct.Pfinal),
-    error('mpt_feasibleStates: Please limit the state-space of interest by Options.Pfinal !');
-end
-bbOptions = Options;
-bbOptions.noPolyOutput = 1; % we don't need the bounding box as a polytope object
-
-if ctrlStruct.probStruct.tracking,
-    [nx,nu]=mpt_sysStructInfo(ctrlStruct.sysStruct);
-    if isa(ctrl, 'mptctrl')
-        nx = ctrl.sysStruct.dims.nx;
-    else
-        nx=(nx-nu)/2;
+    if dimension(Options.Pfinal) ~= nx,
+        error('Wrong dimension of Options.Pfinal');
     end
-    Pfinal = projection(ctrlStruct.Pfinal,1:nx);
+    Pfinal = Options.Pfinal;
+elseif isfulldim(ctrl.Pfinal),
+    Pfinal = ctrl.Pfinal;
 else
-    Pfinal = ctrlStruct.Pfinal;
+    Pfinal = ctrl.Pn;
 end
 
-if length(Pfinal)>1,
-    % if Pfinal is a polyarray, first compute the hull
-    hullPf = hull(Pfinal);
-    % and then calculate the bounding box to obtain bounds on feasible state-space
-    [B, lb, ub] = bounding_box(hullPf,bbOptions);
-else
-    [B, lb, ub] = bounding_box(ctrlStruct.Pfinal,bbOptions);
+if dimension(Pfinal) > nx,
+    Pfinal = projection(Pfinal, 1:nx);
 end
 
-if ctrlStruct.probStruct.tracking,
-    lb = lb(1:nx);
-    ub = ub(1:nx);
-end
-
-% grid the state-space into equidistantly placed points
-%dimB = dimension(B);
-dimB = size(lb(:),1);
-Xpoints = zeros(gridpoints,dimB);
-for ii=1:dimB
-    Xpoints(:,ii) = linspace(lb(ii),ub(ii),gridpoints)';
-end
-    
-
-% generate all possible combinations of states
-% one could use kron() here, but that one fails for high number of elements
-n_states=dimB;
-ZZ=[];
-ZZ{n_states}=Xpoints(:,n_states);
-for ii=n_states-1:-1:1,
-    Zd=[];
-    for jj=1:size(Xpoints,1),
-        Zd=[Zd; repmat(Xpoints(jj,ii),length(ZZ{ii+1}),1)];
-    end
-    ZZ{ii}=Zd;
-end
-for ii=2:n_states,
-    ZZ{ii}=repmat(ZZ{ii},length(ZZ{ii-1})/length(ZZ{ii}),1);
-end
-datapoints=[];
-for ii=1:n_states,
-    datapoints(:,ii)=ZZ{ii};
-end
-
-% datapoints now contains all possible states, but be careful, some of them do
-% not belong to feasible area! 
-
-npoints = size(datapoints,1);
-locOpt = Options;
-locOpt.verbose = -1;
-locOpt.openloop = 0;
-
-cost = zeros(npoints,1);
-
-% break after first region which is being found
-locOpt.fastbreak = 1;
-
-if iscell(ctrlStruct.sysStruct.A),
-    nx = size(ctrlStruct.sysStruct.A{1},2);
-else
-    nx = size(ctrlStruct.sysStruct.A,2);
-end
-
-X0 = [];
-for ii=1:npoints,
-    x0 = datapoints(ii,:)';     % one state vector
-    %if isinside(ctrlStruct.Pn,x0),
-    if isinside(Pfinal,x0),
-        X0 = [X0; x0'];
-    end
-end
+X0 = grid(Pfinal, gridpoints);
